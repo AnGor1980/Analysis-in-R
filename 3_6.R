a01dbff802af6efcc61607a981cc8cff2523d4d5
Andriy Gorshkov <andrey.gorshkov80@gmail.com>
2018-12-12 21:22:32 +0100

    ok

--- 3_6.R
+++ 3_6.R
@@ -4,8 +4,6 @@ smart_hclust<-  function(test_data, cluster_number){
   fit <- hclust(dist(test_data))
   test_data$cluster <- as.factor(cutree(fit,cluster_number))
   test_data
-  
-  
 }
 fit <- hclust(dist(test_data))
 plot(fit)
@@ -67,3 +65,26 @@ get_pca2 <- function(test_data){
   return(test_data)    
 }
 get_pca2(swiss)
+
+#5
+test_data <- read.csv("https://stepic.org/media/attachments/course/524/Norris_1.csv")
+test_data <- read.csv("https://stepic.org/media/attachments/course/524/Norris_2.csv")
+test_data <- read.csv("https://stepic.org/media/attachments/course/524/Norris_3.csv")
+test_data <- as.data.frame(list(V1 = c(16, 7, 12, 2, -2), V2 = c(4, -5, 3, 10, 37), V3 = c(-5, -2, 12, -2, -3), V4 = c(8, -1, 7, 14, 41), V5 = c(12, 9, -5, 9, 10)))
+ct <- cor(test_data)
+ct <- ct - diag(1, ncol(ct)) 
+ct <- round(ct,2)
+ct <- ifelse(abs(ct) == 1.00, row.names(ct), '')
+if(length(ct[ct!=''])==0){
+  "There is no collinearity in the data"
+}
+else{
+  ct[ct!='']
+}
+
+ds <- smart_hclust(swiss, 2)
+library(ggplot2)
+my_plot <- ggplot(ds, aes(Education, Catholic, col = cluster))+
+  geom_point() +
+  stat_smooth(method="lm")
+  
